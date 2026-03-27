a35183a50be374cc99c0ad8ac4fe060dfd60a8f8
celeste-hash <tiagoprlt92@gmail.com>
2026-03-27 16:17:07 +0100

    Update code Tiago.py

---
+++ code Tiago.py
@@ -0,0 +1,10 @@
+def ordre_policies():
+    p1=ch(input("quel est la policy la plus importante parmis : diplomatic, medical, technical ?")) 
+    if p1==diplomatic : 
+        copie{
+    elif p1== medical : 
+
+    elif p1==technical :
+    
+    else : 
+        return("votre policy n'est pas bien orthographiée ou bien pas dans notre liste des priorités")
